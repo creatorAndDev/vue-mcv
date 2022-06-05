5f4b24097be313c9456625ed923630d68ed16839
creatorAndDev <creator.and.dev@gmail.com>
2022-06-05 18:29:47 +0300

    First commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//modules import
+import auth from '@/store/modules/auth'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+
+	},
+	mutations: {
+
+	},
+	actions: {
+		
+	},
+	modules: {
+		auth
+	}
 })
