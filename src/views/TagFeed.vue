a998aa5e96b55c4ea24e4718b97bca2551f7a6d4
creatorAndDev <creator.and.dev@gmail.com>
2022-06-15 18:39:38 +0300

    Added feed Tag and feed Your

---
+++ src/views/TagFeed.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div class="home-page"></div>
+		<mcv-banner />
+		<div class="container page">
+			<div class="row">
+				<div class="col-md-9">
+					<mcv-feed :api-url="apiUrl" />
+				</div>
+				<div class="col-md-3">
+					<mcv-popular-tags />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed'
+import McvPopularTags from '@/components/PopularTags'
+import McvBanner from '@/components/Banner'
+
+export default {
+	name: 'McvTagFeed',
+	components: {
+		McvFeed,
+		McvPopularTags,
+		McvBanner
+	},
+	computed: {
+		apiUrl() {
+			const tagName = this.$route.params.slug;
+			console.log('TagName: ', tagName);
+			return `/articles?tag=${tagName}`
+		}
+	}
+}
+</script>
